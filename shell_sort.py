b597805bdb5e1e1a825369a27e2d4e3b29dbbe76
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2018-11-05 16:33:14 +0530

    shell sort added

---
+++ shell_sort.py
@@ -0,0 +1,31 @@
+'''
+The idea of shellSort is to allow exchange of far items. 
+In shellSort, we make the array h-sorted for a large value of h.
+
+Time Complexity 
+Best : O(n log(n))
+Average : O(n(log(n))^2)
+Worst : O(n(log(n))^2)
+'''
+
+def SHELL_SORT(A):
+	length = len(A)
+	#initalize gap to mid of the array
+	gap = int(length / 2)
+
+	while gap > 0:
+		for i in range(gap, length):
+			temp = A[i]
+			j = i
+
+			while j >= gap and A[j - gap] > temp:
+				A[j] = A[j - gap]
+				j -= gap
+
+			A[j] = temp
+		gap = int(gap / 2)
+
+
+A = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+SHELL_SORT(A)
+print(A)
