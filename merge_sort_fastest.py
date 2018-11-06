da5c27a875497226c579892435507d8483764483
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2018-11-06 20:23:34 +0530

    merge sort fastest added

--- merge_sort_fastest.py
+++ merge_sort_fastest.py
@@ -1,16 +1,16 @@
-def MERGE_SORT(LIST):
+def MERGE_SORT(A):
     start = []
     end = []
-    while len(LIST) > 1:
-        a = min(LIST)
-        b = max(LIST)
+    while len(A) > 1:
+        a = min(A)
+        b = max(A)
         start.append(a)
         end.append(b)
-        LIST.remove(a)
-        LIST.remove(b)
-    if LIST: start.append(LIST[0])
+        A.remove(a)
+        A.remove(b)
+    if A: start.append(A[0])
     end.reverse()
     return (start + end)
 
-A = [1, 2, 3, 9, 8, 7, 6, 5]
+A = [9, 8, 7, 6, 5, 4, 3, 2, 1]
 print(MERGE_SORT(A))
