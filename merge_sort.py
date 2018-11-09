f22581bccbe08c6495c3024ee0b520994e2f8234
bhushan-borole <37565807+bhushan-borole@users.noreply.github.com>
2018-11-09 10:32:15 +0530

    merge sort without merge added

--- merge_sort.py
+++ merge_sort.py
@@ -26,8 +26,9 @@ def mergeSort(A,p,r):
         mergeSort(A,q,r)
         MERGE(A,p,q,r)
 
-A  = [20, 30, 21, 15, 42, 45, 31, 0, 9]
+A = [9, 8, 7, 6, 5, 4, 3, 2, 1]
 mergeSort(A,0,len(A))
 print(A)
+
 
 
